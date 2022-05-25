fe761cffb7a1593db46d4ee298932e430be60ee4
Jellen <un5450@naver.com>
2022-05-26 02:49:31 +0900

    remove unnecessary structure comments

--- lua/starryadmins/sh_permissions.lua
+++ lua/starryadmins/sh_permissions.lua
@@ -1,33 +1,9 @@
 local perm = {}
 StarryAdmins.perm = perm
 StarryAdmins.authorityData = StarryAdmins.authorityData or {}
---[[
-    StarryAdmins.authorityData contains tag and usergroup informations.
-    StarryAdmins.authorityData structure
-    {
-        [tagName or usergroupName] = {
-            displayName = string,
-            perm = {
-                [perm] = (true or false or nil)
-            },
-        },
-    }
-]]--
-
---[[
-    Player.starryTags structure
-    {
-        [tagName] = true,
-    }
-]]--
 
 StarryAdmins.netsync.register("authorityData", "authorityData", false)
 StarryAdmins.netsync.register("starryTags", "starryTags", true)
---[[
-    == Permission System ==
-    A user's permissions are union of their group's permissions and their tags' permissions.
-    A user can only have one usergroup, but can have multiple tags.
-]]--
 
 -- Player permission system
 do
